1cb5355ab34103bbc6452c2100dcbc3b0475fb91
AdityaGuptaa92 <adityagupta3214@gmail.com>
2020-06-16 17:40:11 +0530

    Added top and bottom animation and implemented Splash Screen

--- app/src/main/java/com/aditya/tictactoe/MainActivity.kt
+++ app/src/main/java/com/aditya/tictactoe/MainActivity.kt
@@ -1,13 +1,36 @@
 package com.aditya.tictactoe
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+
+        var imageView: ImageView = findViewById(R.id.imageView)
+        var textView: TextView = findViewById(R.id.textView)
+
+        var topAnim = AnimationUtils.loadAnimation(this, R.anim.top_anim)
+        var bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim)
+
+        imageView.startAnimation(topAnim)
+        textView.startAnimation(bottomAnim)
+
+        Handler().postDelayed({
+            val intent = Intent(this, Dashboard::class.java)
+            startActivity(intent)
+            finish()
+        }, 3600)
+
     }
 }
