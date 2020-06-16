94c285ae11fd17464bba6a34166155fd644fd011
AdityaGuptaa92 <adityagupta3214@gmail.com>
2020-06-16 13:11:10 +0530

    Removed action, status bar and added a image

--- app/src/main/java/com/aditya/tictactoe/MainActivity.kt
+++ app/src/main/java/com/aditya/tictactoe/MainActivity.kt
@@ -2,10 +2,12 @@ package com.aditya.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
     }
 }
